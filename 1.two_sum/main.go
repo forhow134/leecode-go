0b801c2e3f49bf858dc37613b2a68d2d7638c1d8
雷猴 <monitoring@gzleihou.cn>
2022-01-16 22:36:38 +0800

    完成第三题

---
+++ 1.two_sum/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum([]int{3, 3}, 6))
+}
+
+//提交部分
+func twoSum(nums []int, target int) []int {
+	n := len(nums)
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
